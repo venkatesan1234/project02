41dd509a5c908c8b5be91a8ad70d48513de97767
venkatesan1234 <venkatesanben0@gmail.com>
2022-04-20 20:00:37 +0530

    venkyy's code

--- src/test/java/projectt/Project011.java
+++ src/test/java/projectt/Project011.java
@@ -2,9 +2,9 @@ package projectt;
 
 public class Project011 {
 public static void main(String[] args) {
-	System.out.println("test1");
-	System.out.println("test3");
-	System.out.println("venkatesan");
+	System.out.println("test5");
+	System.out.println("test4");
+	System.out.println("venkat");
 	System.out.println("test5");
 }
 }
